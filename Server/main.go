8557064fa2d3633b30b6fe6e3760ad4d682b65dd
LavishRawat <lavish.rawat@coloredcow.in>
2022-11-21 11:07:54 +0530

    Backend setup with Database Connection

--- Server/main.go
+++ Server/main.go
@@ -1,22 +1,61 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+
+	// "os"
 	"fmt"
 	"log"
-	"net/http"
-	"Server/router"
 
-	// "gorm.io/driver/postgres"
-  	// "gorm.io/gorm"
+	_ "github.com/lib/pq"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+type SignUp struct{
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	FirstName string 	`gorm:"not null;" json:"firstname"`
+	LastName  string 	`gorm:"not null;" json:"lastname"`
+	Email     string	`gorm:"not null;" json:"email"`
+	Password  uint		`gorm:"not null;" json:"password"`
+	ConPassword  uint	`gorm:"not null;" json:"conpassword"`
+}
+
+const (
+    HOST = "localhost"
+    PORT = 5432
+    USER = "postgres"
+    PASSWORD = "Lavish@123"
+    DBNAME = "react_app"
+)
+
+func initializerRouter() {
+	r := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":9000",r))
+
+
+}
+
+
 func main() {
-	r := router.Router()
-	fmt.Println("Starting the server on Port 9000...")
+	connString := fmt.Sprintf(
+        "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+        HOST, PORT, USER, PASSWORD, DBNAME,
+    )
+    DB, err := gorm.Open(postgres.Open(connString), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	log.Fatal(http.ListenAndServe(":9000", r))
+	DB.AutoMigrate(&SignUp{})
+	
+	fmt.Println("Successfully connected!")
+	// connect()
 
-
-	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	// _, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	initializerRouter()
 }
